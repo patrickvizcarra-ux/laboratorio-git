7f585b38b65734bcb49fe1127548eaea9cfba8f9
Patrick <patrick@MacBook-Air-de-Patrick.local>
2026-08-24 17:27:10 -0500

    Agrega operacion de multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -8,7 +8,12 @@ public class Calculadora {
         return a - b;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Operaciones disponibles: suma, resta");
+    public static int multiplicar(int a, int b) {
+        return a * b;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Operaciones disponibles: suma, resta, multiplicacion");
+    }
+
 }
