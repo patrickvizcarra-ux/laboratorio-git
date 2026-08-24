4ba343ae9c6e935f2ac399a55b19b02552647034
Patrick <patrick@MacBook-Air-de-Patrick.local>
2026-08-24 17:24:01 -0500

    Agrega operacion de resta

--- Calculadora.java
+++ Calculadora.java
@@ -4,12 +4,11 @@ public class Calculadora {
         return a + b;
     }
 
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Operaciones disponibles: suma");
-
-        int a = 10;
-        int b = 5;
-
-        System.out.println("Suma: " + sumar(a, b));
+        System.out.println("Operaciones disponibles: suma, resta");
     }
 }
